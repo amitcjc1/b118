00395958482cb8f9c34a1af2ef0fad28b33ae0c7
amit <amit@DESKTOP-SIU2A5U.mshome.net>
2020-09-04 18:58:21 +0530

    by CJC

--- gitDemo/src/gitDemo/Demo.java
+++ gitDemo/src/gitDemo/Demo.java
@@ -2,6 +2,12 @@ package gitDemo;
 
 public class Demo 
 {
+	
+	public void sum()
+	{
+		System.out.println("Summ method");
+	}
+	
 public static void main(String[] args) {
 	
 	System.out.println("Hello Main method");
